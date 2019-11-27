362fe836dd292628b11dd7f7b5581847a2ff9bce
Jason Koo <jkoo2088@gmail.com>
2019-11-27 00:14:39 -0500

    updated file

--- lab5/src/app/app.module.ts
+++ lab5/src/app/app.module.ts
@@ -2,12 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 //import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { RouterModule} from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SongCreateComponent } from './components/songs/song-create/song-create.component';
 import { OpenHomeComponent} from './components/home/open-home/open-home.component';
 import { AdminHomeComponent } from './components/home/admin-home/admin-home.component';
+import { AuthHomeComponent } from './components/home/auth-home/auth-home.component';
+import { LoginComponent } from './components/login/login.component';
 
 
 @NgModule({
@@ -15,12 +18,18 @@ import { AdminHomeComponent } from './components/home/admin-home/admin-home.comp
     AppComponent,
     SongCreateComponent,
     OpenHomeComponent,
-    AdminHomeComponent
+    AdminHomeComponent,
+    AuthHomeComponent,
+    LoginComponent
 
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      { path: 'api/open', component: OpenHomeComponent },
+      { path: 'login', component: LoginComponent }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
