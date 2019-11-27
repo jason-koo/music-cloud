7c8707cb8d960fe7f2fab0a6ab679a74d2c323cd
Jason Koo <jkoo2088@gmail.com>
2019-11-26 23:28:00 -0500

    updated files

--- lab5/src/app/app.module.ts
+++ lab5/src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+//import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SongsComponent } from './components/songs/songs.component';
+import { SongCreateComponent } from './components/songs/song-create/song-create.component';
+import { OpenHomeComponent} from './components/home/open-home/open-home.component';
+import { AdminHomeComponent } from './components/home/admin-home/admin-home.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    SongsComponent,
-    
+    SongCreateComponent,
+    OpenHomeComponent,
+    AdminHomeComponent
+
   ],
   imports: [
     BrowserModule,
