f3584a261d26f543445d00c96a2b133c22a5a980
Jason Koo <jkoo2088@gmail.com>
2019-11-28 20:51:55 -0500

    updated auth service

--- lab5/src/app/services/auth.service.ts
+++ lab5/src/app/services/auth.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  private registerURL = "http://localhost:8080/api/open/register";
+  private loginURL =  "http://localhost:8080/api/open/login";
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+
+  registerUser(user) {
+    return this.http.post<any>(this.registerURL, user)
+  }
+
+  loginUser(user) {
+    return this.http.post<any>(this.loginURL, user);
+  }
 }
