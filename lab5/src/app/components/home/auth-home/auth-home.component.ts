458a4838c9c74416bdced3a848294d4506c021d0
Jason Koo <jkoo2088@gmail.com>
2019-12-01 15:44:26 -0500

    updated home pages

--- lab5/src/app/components/home/auth-home/auth-home.component.ts
+++ lab5/src/app/components/home/auth-home/auth-home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpService } from '../../../services/http.service';
+import { AuthService } from '../../../services/auth.service'
 
 @Component({
   selector: 'app-auth-home',
@@ -7,9 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AuthHomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _http:HttpService, private _authSerivce:AuthService) { }
 
   ngOnInit() {
+    this._http.getAuthUserPage().subscribe(
+      res => console.log(res),
+      err => console.log(err)
+    )
   }
 
 }
--- lab5/src/app/components/home/open-home/open-home.component.ts
+++ lab5/src/app/components/home/open-home/open-home.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../../services/auth.service'
+import { HttpService } from '../../../services/http.service';
+import { Song } from '../../../models/song';
 
 @Component({
   selector: 'app-open-home',
   templateUrl: './open-home.component.html',
-  styleUrls: ['./open-home.component.css']
+  styleUrls: ['./open-home.component.css'],
+ 
 })
 export class OpenHomeComponent implements OnInit {
 
-  constructor() { }
+  searchData = {}
+  constructor(private _authService:AuthService, private _http:HttpService) { }
 
   ngOnInit() {
+  }
+
+  findSong(){
+    this._http.findTheSong(this.searchData["song"]).subscribe(
+      res => {
+        this._http.songs = res as Song[];
+        console.log(res)
+      },
+      err => console.log(err)
+      
+    )
   }
 
 }
