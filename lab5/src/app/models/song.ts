e696a62ca7c52f7a60031f2434855302242341d0
Jason Koo <jkoo2088@gmail.com>
2019-12-01 09:32:21 -0500

    created new models

---
+++ lab5/src/app/models/song.ts
@@ -0,0 +1,12 @@
+export class Song {
+    _id:number;
+    objectID: 5;
+    title: string;
+    artist: string;
+    album: string;
+    year: number;
+    comment: string;
+    genre: string;
+    avgRating: number;
+    numOfReviews: number;
+}
