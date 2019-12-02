7d6f0f11c854d4964cb917b4c5896bcac2dbec86
Jason Koo <jkoo2088@gmail.com>
2019-12-02 13:37:10 -0500

    added method to show all users

--- lab5/src/app/services/show-users.service.ts
+++ lab5/src/app/services/show-users.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { User } from '../models/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ShowUsersService {
 
-  constructor() { }
+  selectedUser: User;
+  users: User[]
+  private showUsersURL = "http://localhost:8080/api/admin/users"
+  constructor(private _http:HttpClient) { }
+
+  getUsers() {
+    return this._http.get(this.showUsersURL);
+  }
 }
